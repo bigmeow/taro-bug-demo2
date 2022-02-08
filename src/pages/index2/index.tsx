7d94cacebafec5fd8f8f2c62ea5e324bc01310f4
bigMeow <running@vip.163.com>
2022-02-08 16:07:52 +0800

    feat: bug重现

---
+++ src/pages/index2/index.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index2 extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index2'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
